04743f2615c35accbdd49fb7a2c4e6a6cbf3de42
pramit-marattha <pramitmarattha5497@gmail.com>
2021-02-22 23:12:36 +0545

    initializing the app

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Account Record App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <h1 className="text-6xl text-red-500">Account Record</h1>
+    </div>
+  )
+}
